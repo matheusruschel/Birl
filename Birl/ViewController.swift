af7a3916b2efa66069f7e818ec273c0dd2e1ccac
Matheus Ruschel <matheus.ruschel@gmail.com>
2016-07-21 16:03:09 -0300

    corrected typo in audio files array

--- Birl/ViewController.swift
+++ Birl/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     let audioPlayer = AudioPlayer.sharedInstance
     
     let audioFiles = ["birl","show","que-num-vai-da-pai","bambam","porra_10","13-memo","trapezio-descendente_1","37-anos","iiiiiiuuuuuuuu-bmabma","bambam-enlouquecendo-no-treino-com-felipe-franco-2","trapezio-descendente-e-o-nome","quero-mais-quero-mais", "bam_bam", "eita-porra_1","bambam-_aqui-e-bodybuilding-porra_-360p","bambam-aqui-e-bodybuilding-porra-mp3cut"]
-    let audioNames = ["Birl","Hora do show","Que não vai da","Saí de casa, comi pra...","Super sayajin","É 13 po","Trapézio descendente","É 37 anos","Iiiiiiuuuuuuuu","Ta saindo da jaula","Trapézio descendente 2","Quero mais quero main", "Sai fdp", "Eita porra","Aqui é bodybuilder (Completo)","Aqui é bodybuilder"]
+    let audioNames = ["Birl","Hora do show","Que não vai da","Saí de casa, comi pra...","Super sayajin","É 13 po","Trapézio descendente","É 37 anos","Iiiiiiuuuuuuuu","Ta saindo da jaula","Trapézio descendente 2","Quero mais quero mais", "Sai fdp", "Eita porra","Aqui é bodybuilder (Completo)","Aqui é bodybuilder"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
